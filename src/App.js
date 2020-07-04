aee36d22b9a94189643a345a7b58a6cfe294fd55
ojy2049 <ojy2049@hanmail.net>
2020-07-04 23:17:12 +0900

    reat component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer'
+
+// 프롭스 작성으로 구조화되어 웹문서 출력 가능.
+// 프롭스만 수정해주어도 반영되어 출력
+const customer = {
+  'name' : '왈왈이',
+  'birthday' : '961222',
+  'gender' : '남자',
+  'job' : '대학생'
+}
+
 
 function App() {
   return ( 
-  <div className = "gray-background">
-    <img src = {logo} lat = "logo"></img>  
-    <h2> Let's develop management system</h2> 
-    </div>
+  <Customer
+    name = {customer.name}
+    birthday = {customer.birthday}
+    gender = {customer.gender}
+    job = {customer.job}
+    />
   );
 }
 
