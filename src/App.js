208968b5cf7bc66bc128ad6a84a131385499a522
ojy2049 <ojy2049@hanmail.net>
2020-07-06 01:48:20 +0900

    apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer'
+import Table from '@material-ui/core/Table'
+import TableHead from '@material-ui/core/TableHead'
+import TableBody from '@material-ui/core/TableBody'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+import { withStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+
+const styles = theme => ({
+  root : {
+    width : '100%',
+    marginTop : theme.spacing.unit * 3,
+    overflowX : "auto",
+
+  },
+  table : {
+    minWidth : 1080
+  }
+});
 
 // 프롭스 작성으로 구조화되어 웹문서 출력 가능.
 // 프롭스만 수정해주어도 반영되어 출력
 const customers = [
 {
   'id' : 1,
-  'image' : 'http://placeimg.com/64/64/any',
+  'image' : 'http://placeimg.com/64/64/1',
   'name' : '왈왈이',
   'birthday' : '961222',
   'gender' : '남자',
@@ -16,7 +35,7 @@ const customers = [
 },
 {
   'id' : 2,
-  'image' : 'http://placeimg.com/64/64/any',
+  'image' : 'http://placeimg.com/64/64/2',
   'name' : '헬로우',
   'birthday' : '961322',
   'gender' : '남자',
@@ -24,36 +43,39 @@ const customers = [
 },
 {
   'id' : 3,
-  'image' : 'http://placeimg.com/64/64/any',
+  'image' : 'http://placeimg.com/64/64/3',
   'name' : '댕댕댕',
   'birthday' : '961522',
   'gender' : '남자',
   'job' : '대학생'
 }
 
-]
+];
 
 
-function App() {
-  return ( 
-    <div>
-  {
-    customers.map(c=> {
-      return (
-        <Customer
-          key={c.id}
-          id = {c.id}
-          image={c.image}
-          name={c.name}
-          birthday={c.birthday}
-          gender={c.gender}
-          job={c.job}
-        />
-      )
-    })
+class App extends Component {
+  render() {
+    const { classes } = this.props;
+    return ( 
+      <Paper className = {classes.root}>
+        <Table className = {classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c=> { return ( <Customer key={c.id} id = {c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ) })}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
   }
-    </div>
-  );
 }
 
-export default App;
+export default withStyles(styles)(App);
