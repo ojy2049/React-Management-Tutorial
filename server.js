dcd8e034d4a8b1d3a81d0229818f4a4db2999fa0
ojy2049 <ojy2049@hanmail.net>
2020-07-16 02:16:54 +0900

    Create Customer DB Table & Node.js DB Connection Setting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,35 +7,28 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host : conf.host,
+  user : conf.user,
+  password : conf.password,
+  port : conf.port,
+  database : conf.database
+});
+
+connection.connect();
+
 // 사용자 입장에서 api/hello 로 접근 시 무슨 처리를 해줄건지
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id' : 1,
-            'image' : 'http://placeimg.com/64/64/1',
-            'name' : '왈왈이',
-            'birthday' : '961222',
-            'gender' : '남자',
-            'job' : '대학생'
-          },
-          {
-            'id' : 2,
-            'image' : 'http://placeimg.com/64/64/2',
-            'name' : '헬로우',
-            'birthday' : '961322',
-            'gender' : '남자',
-            'job' : '대학생'
-          },
-          {
-            'id' : 3,
-            'image' : 'http://placeimg.com/64/64/3',
-            'name' : '댕댕댕',
-            'birthday' : '961522',
-            'gender' : '남자',
-            'job' : '대학생'
-          }
-          
-    ]);
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err, rows, fields) => {
+        res.send(rows);
+      }
+    );
 });
 
 // 서버가 동작 중이면 동작 중 이라고 출력 (포트번호를 출력)(따옴표가 아니라 숫자 1 왼쪽에 있는 특수문자임)
